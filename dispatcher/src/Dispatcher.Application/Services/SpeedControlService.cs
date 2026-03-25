315e65a4050f7ff4b8e083b770f641b7ded18acc
farukomerg <guleromerfaruk10@gmail.com>
2026-03-25 08:50:23 +0300

    İlk test yazıldı

---
+++ dispatcher/src/Dispatcher.Application/Services/SpeedControlService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispatcher.Application.Services
+{
+    public class SpeedControlService
+    {
+
+        public bool IsSpeedLimitExceeded(int speed)
+        {
+            return speed > 90;
+        }
+    }
+}
