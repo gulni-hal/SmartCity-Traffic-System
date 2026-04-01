ada8d59acb3660b580fd8a08096465bb24362855
farukomerg <guleromerfaruk10@gmail.com>
2026-04-01 18:18:36 +0300

    Dispetchar'da downstream servis hatası için 502 testi yazıldı red

---
+++ dispatcher/tests/Dispatcher.IntegrationTests/DownstreamErrorTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Http.Headers;
+using Dispatcher.Application;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Dispatcher.IntegrationTests;
+
+public class DownstreamErrorTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public DownstreamErrorTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Get_Traffic_With_Valid_Token_And_Unavailable_Service_Should_Return_502()
+    {
+        var factory = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll(typeof(IAuthValidationService));
+                services.AddSingleton<IAuthValidationService>(
+                    new FakeAuthValidationService(new AuthValidationResult
+                    {
+                        IsValid = true,
+                        Username = "ali",
+                        Role = "TrafficPolice"
+                    }));
+            });
+        });
+
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", "valid-token");
+
+        var response = await client.GetAsync("/api/traffic/live");
+
+        Assert.Equal(HttpStatusCode.BadGateway, response.StatusCode);
+    }
+
+    private sealed class FakeAuthValidationService : IAuthValidationService
+    {
+        private readonly AuthValidationResult _result;
+
+        public FakeAuthValidationService(AuthValidationResult result)
+        {
+            _result = result;
+        }
+
+        public Task<AuthValidationResult> ValidateAsync(string token)
+        {
+            return Task.FromResult(_result);
+        }
+    }
+}
+
